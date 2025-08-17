835702d09f3972c2e4d497ab9e167aa80e98d956
Bruno Pessoa <brunofernpessoa@gmail.com>
2025-08-17 15:42:17 -0300

    feat: cria componente da tela inicial da Bíblia

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { BibleScreen } from './src/ui/screens/BibleScreen';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
-
   return (
     <View style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      <BibleScreen />
     </View>
   );
 }
---
+++ src/ui/screens/BibleScreen.tsx
@@ -0,0 +1,57 @@
+import { ScrollView, View } from 'react-native';
+import { useState } from 'react';
+import DropDownPicker from 'react-native-dropdown-picker';
+import { useBible } from '../../presentation/BibleState';
+import { BibleVerse } from '../components/BibleVerse';
+import { useCases } from '../../application/factories/UseCases';
+
+export function BibleScreen() {
+  const [isBookDropDownOpen, setIsBookDropDownOpen] = useState(false);
+  const [isChapterDropDownOpen, setIsChapterDropDownOpen] = useState(false);
+
+  const bookId = useBible(s => s.selectedBookId);
+  const chapterId = useBible(s => s.selectedChapterId);
+  const setBook = useBible(s => s.setSelectedBookId);
+  const setChapter = useBible(s => s.setSelectedChapterId);
+
+  DropDownPicker.setLanguage('PT');
+
+  function bindDropDown<T>(stateValue: T, zustandSetter: (v: T) => void) {
+    return (val: T | ((curr: T) => T)) => {
+      const newValue =
+        typeof val === 'function' ? (val as (curr: T) => T)(stateValue) : val;
+      zustandSetter(newValue);
+    };
+  }
+
+  return (
+    <View>
+      <View>
+        <DropDownPicker
+          closeAfterSelecting
+          searchable
+          open={isBookDropDownOpen}
+          value={bookId}
+          items={useCases.bible.getBibleBookList()}
+          setOpen={setIsBookDropDownOpen}
+          setValue={bindDropDown(bookId, setBook)}
+        />
+
+        <DropDownPicker
+          searchable
+          closeAfterSelecting
+          open={isChapterDropDownOpen}
+          value={chapterId}
+          items={useCases.bible.getBibleChapterList(bookId)}
+          setOpen={setIsChapterDropDownOpen}
+          setValue={bindDropDown(chapterId, setChapter)}
+        />
+      </View>
+      <ScrollView>
+        {useCases.bible.getBibleVerses(bookId, chapterId).map(v => (
+          <BibleVerse key={v.id} verse={v} />
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
